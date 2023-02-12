f739ce3099c385671aa6ee81f30b49734c0e13b9
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-02-12 20:57:16 +0100

    Add newProxyFromCSV, making it private and adding doc

--- internals/proxy/proxy.go
+++ internals/proxy/proxy.go
@@ -1,6 +1,7 @@
 package proxy
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -15,46 +16,55 @@ type Proxy struct {
 	protocols []string
 }
 
-// NewHTTPProxy returns a HTTP proxy with the ip and the port sepcified as argument
-func NewHTTPProxy(host string, port string) Proxy {
-	return Proxy{host, port, []string{"HTTP"}}
-}
-
-// NewProxy returns a proxy with the data passed as argument
-func NewProxy(ip string, port string, protocols []string) Proxy {
+// newProxy returns a proxy with the data passed as argument
+func newProxy(ip string, port string, protocols []string) Proxy {
 	return Proxy{ip, port, protocols}
 }
 
-// NewHTTPProxyFromIP returns an HTTP Proxy from a string with the IP host:port.
-// For example 103.119.95.106:80
-func NewHTTPProxyFromIP(ip string) Proxy {
-	splitedIP := strings.Split(ip, ":")
-	host := splitedIP[0]
-	port := splitedIP[1]
-
-	return NewHTTPProxy(host, port)
+// NewProxyFromCSV returns a proxy from a CSV line with the next format:
+// Protocol, IP, Port.
+// If the string doesn't have this format, it returns an error
+// For example, given:
+// HTTP, 192.168.1.100, 8080
+// it returns:
+//
+//	Proxy{
+//	  ip:192.168.1.100
+//	  port:8080
+//	  protocols:["HTTP"]
+//	}
+func newProxyFromCSV(csv string) (Proxy, error) {
+	csvSplitted := strings.Split(csv, ",")
+	if len(csvSplitted) < 2 {
+		return Proxy{}, errors.New("invalid CSV line. It has to have th next format: Protocols,IP,Port")
+	}
+	return Proxy{
+		ip:        csvSplitted[1],
+		port:      csvSplitted[2],
+		protocols: []string{csvSplitted[0]},
+	}, nil
 }
 
 // getPort is the port getter. Returns a string with the available port of the proxy
-func (p Proxy) getPort() string {
+func (p Proxy) GetPort() string {
 	return p.port
 }
 
 // getHost is the host getter. Returns a string with the host of the proxy
-func (p Proxy) getIP() string {
+func (p Proxy) GetIP() string {
 	return p.ip
 }
 
 // Parse returns a string with the Proxy info.
-// For example: http:// 103.119.95.106:80
+// For example: http://103.119.95.106:80
 func (p Proxy) Parse() string {
 	return fmt.Sprintf("%s://%s:%s", p.protocols[0], p.ip, p.port)
 }
 
-// Check checks whether the proxy works properly.
+// check checks whether the proxy works properly.
 // If it doesn't it prints the error or the status response
 // To checks this makes a simple request to http://ip-api.com/json/
-func (p *Proxy) Check() bool {
+func (p *Proxy) check() bool {
 	resp, err := p.makeSimpleReq()
 	if err != nil || resp.StatusCode != 200 {
 		return false
@@ -76,9 +86,9 @@ func (p Proxy) makeSimpleReq() (*http.Response, error) {
 	return client.Do(req)
 }
 
-// ToCSV returns the a string with the proxy information in CSV fromat with an line jump at the end.
+// toCSV returns the a string with the proxy information in CSV fromat with an line jump at the end.
 // For example:
 // "HTTP, 192.168.1.100, 8080 \n"
-func (p Proxy) ToCSV() string {
+func (p Proxy) toCSV() string {
 	return p.protocols[0] + "," + p.ip + "," + p.port + "\n"
 }
