853892a6b7e43db6744112180875af325b0de00d
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-02-14 15:33:54 +0100

    Revert "Testing"
    This reverts commit 98d5d5894b280cbeee7be1b03c4e2890e3184f9d.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,28 +1,7 @@
 package main
 
-import (
-	"sync"
-
-	"github.com/lucastomic/zalando-bot/internals/logger"
-	"github.com/lucastomic/zalando-bot/internals/proxy"
-)
+import "github.com/lucastomic/zalando-bot/internals/logger"
 
 func main() {
-
-	it, _ := proxy.NewIterator()
-	it.RefreshProxies()
-	for i := 0; i < 5; i++ {
-		var wg sync.WaitGroup
-		for i := 0; i < 10; i++ {
-			wg.Add(1)
-			go func() {
-				logger.SignIn("lucastomic17@gmail.com", "94039155")
-
-				wg.Done()
-			}()
-		}
-		// time.Sleep(time.Hour)
-
-		wg.Wait()
-	}
+	logger.SignIn()
 }
